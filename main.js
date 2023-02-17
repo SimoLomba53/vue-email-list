c0644dace42c23e19b22658f025480ac774e21ed
SimoLomba53 <simolomba253@gmail.com>
2023-02-17 16:08:41 +0100

    10 rndm mail pushate nell'array e in console

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ randommail = [];
 for (let i = 0; i < 10; i++) {
   axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(function (results) {
-      console.log(results);
+    .then((result) => {
+      randommail.push(result.data.response);
+
+      for (let i = 0; i <= 10; i++) {
+        console.log(result.data.response);
+      }
     });
 }
