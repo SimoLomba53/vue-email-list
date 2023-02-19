203ab7b0c8a045be071a0294d441616426ddde50
SimoLomba53 <simolomba253@gmail.com>
2023-02-19 22:19:53 +0100

    problemi con array e nel farne uscire 10 insieme

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 //https://flynn.boolean.careers/exercises/api/random/mail
 
-randommail = [];
+//randommail = [];
 
-for (let i = 0; i < 10; i++) {
-  axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((result) => {
-      randommail.push(result.data.response);
+//for (let i = 0; i < 10; i++) {
+//axios
+//.get("https://flynn.boolean.careers/exercises/api/random/mail")
+//.then((result) => {
+//randommail.push(result.data.response);
 
-      for (let i = 0; i <= 10; i++) {
+//for (let i = 0; i <= 10; i++) {
+//console.log(result.data.response);
+//}
+//});
+//}
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      mail: [],
+    };
+  },
+
+  methods: {
+    addmail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => this.mail.push(result.data.response));
+
+      if (mail.length >= 10) {
         console.log(result.data.response);
       }
-    });
-}
+    },
+  },
+}).mount("#app");
